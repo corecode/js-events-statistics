3ca5cd1ce1ae07cc15a5e948c14022c86be7e539
Simon Schubert <2@0x2c.org>
2013-01-25 19:24:14 +0100

    visual output

--- statistics.js
+++ statistics.js
@@ -1,15 +1,42 @@
 (function() {
     var log2 = Math.log(2);
+    var histNode;
     var timeHist = [];
 
-    var histNode;
-    injectHistogram = function() {
+    var TimeBucket = function(bucket_) {
+        this.bucket = bucket_;
+        this.count = 0;
+
+        if (typeof(histNode) === 'undefined') {
+            injectHistogram();
+        }
+
+        var node = document.createElement("div");
+        var label = document.createElement("span");
+        label.innerHTML = this.bucket + ": ";
+        node.appendChild(label);
+        this.content = document.createElement("span");
+        node.appendChild(this.content);
+        histNode.appendChild(node);
+        this.updateDisplay();
+    };
+    TimeBucket.prototype = {
+        register: function() {
+            this.count += 1;
+            this.updateDisplay();
+        },
+        updateDisplay: function() {
+            this.content.innerHTML = this.count;
+        }
+    };
+
+    var injectHistogram = function() {
         if (typeof(histNode) !== 'undefined') {
             return;
         }
 
         histNode = document.createElement("div");
-        histNode.className = "0x2c-timeout-histogram";
+        histNode.id = "js-event-stats-0x2c";
         histNode.innerHTML = "foo";
         document.body.appendChild(histNode);
     };
@@ -17,42 +44,28 @@
     var old_setInterval = window.setInterval;
     window.setInterval = function(callback, timeout)
     {
-        var caller = arguments.callee.caller;
-
-        console.log("setInterval:" +
-                    " caller: " + caller.name +
-                    " callback: " + callback.name +
-                    " timeout: " + timeout);
         return (old_setInterval(callback, timeout));
     };
 
     var old_setTimeout = window.setTimeout;
     window.setTimeout = function(callback, timeout)
     {
-        var caller = arguments.callee.caller;
-
-        console.log("setTimeout:" +
-                    " caller: " + caller.name +
-                    " callback: " + callback.name +
-                    " timeout: " + timeout);
-
         var wrappedCallback = function() {
             var startTime = performance.now();
             callback();
             var endTime = performance.now();
             var runtime = (endTime - startTime) * 1000;
-            var timebucket = Math.log(runtime) / log2;
+            var timebucket = Math.round(Math.log(runtime) / log2);
 
             if (timebucket < 0) {
                 timebucket = 0;
             }
 
-            timeHist[timebucket] = (timeHist[timebucket] || 0) + 1;
-            injectHistogram();
-            console.log("timeout callback " +
-                        " from " + caller.name +
-                        " to " + callback.name +
-                        " duration " + (endTime - startTime));
+            if (typeof(timeHist[timebucket]) === 'undefined') {
+                timeHist[timebucket] = new TimeBucket(timebucket);
+            }
+
+            timeHist[timebucket].register();
         };
         return (old_setTimeout(wrappedCallback, timeout));
     };
